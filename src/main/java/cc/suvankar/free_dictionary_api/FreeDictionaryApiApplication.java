54969139959763fe40849a8bc4fa37525e5310ec
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2026-02-05 20:15:44 +0530

    Enable JPA repositories for the application to fix the repository config clash between JPA and Redis

--- src/main/java/cc/suvankar/free_dictionary_api/FreeDictionaryApiApplication.java
+++ src/main/java/cc/suvankar/free_dictionary_api/FreeDictionaryApiApplication.java
@@ -3,9 +3,11 @@ package cc.suvankar.free_dictionary_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
+@EnableJpaRepositories(basePackages = "cc.suvankar.free_dictionary_api.repository")
 public class FreeDictionaryApiApplication {
 
 	public static void main(String[] args) {
